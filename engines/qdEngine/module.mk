9025be51ff6b7c83b42c7f4297468da7155d6bf7
kunxl-gg <tiwari.25@iitj.ac.in>
2024-04-04 22:17:44 +0530

    QDENGINE: Added engine code
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>
    
    removed .jukit
    
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- engines/qdEngine/module.mk
+++ engines/qdEngine/module.mk
@@ -4,7 +4,8 @@ MODULE_OBJS = \
 	qdengine.o \
 	console.o \
 	metaengine.o \
-	detection.o
+	detection.o \
+	core/qd_precomp.o
 
 # This module can be built as a plugin
 ifeq ($(ENABLE_QDENGINE), DYNAMIC_PLUGIN)
